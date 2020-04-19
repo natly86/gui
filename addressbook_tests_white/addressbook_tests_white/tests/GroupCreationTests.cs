f81fcbdc3e6aa3d8a77d7de884336a52a0d14a37
Malysheva_NU <Malysheva_NU@nposapfir.ru>
2020-04-19 19:09:03 +0500

    Задание №18: Реализовать тесты для добавления и удаления групп

---
+++ addressbook_tests_white/addressbook_tests_white/tests/GroupCreationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "white"
+            };
+
+            app.Groups.Add(newGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Sort();
+            newGroups.Sort();
+            
+            Assert.AreEqual(oldGroups.Count + 1, newGroups.Count);
+        }
+    }
+}
---
+++ addressbook_tests_white/addressbook_tests_white/tests/GroupRemovalTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace addressbook_tests_white
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemove()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            GroupData newGroup = new GroupData()
+            {
+                Name = "test"
+            };
+
+            app.Groups.Remove(newGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            Assert.AreEqual(oldGroups, newGroups);
+        }
+    }
+}
